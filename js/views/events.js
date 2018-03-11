a70abf806d34c46753e5561957f27f5d7ce80809
Chronotron <paulchronotron@gmail.com>
2018-03-11 10:50:02 -0500

    events page

---
+++ js/views/events.js
@@ -0,0 +1,16 @@
+
+function searchCalendar() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("calendarSearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("calendarList");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
